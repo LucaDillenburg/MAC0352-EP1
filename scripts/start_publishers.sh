4142277b60c8245982a8908bd229072f5b0f8711
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-04-26 05:47:42 -0300

    improve

--- scripts/start_publishers.sh
+++ scripts/start_publishers.sh
@@ -9,7 +9,7 @@ i=1
 while [ "$i" -le "$1" ]; do
     m="$(($i % $2))"
     echo "Started mosquitto_pub in topic 'topic_$m'"
-    ./mosquitto_pub -t "topic_$m" -m "Hello World" -h 10.0.0.101 &
+    ./mosquitto_pub -t "topic_$m" -m "Hello World from topic_$m at $(date +"%T")" -h 10.0.0.101 &
     i=$((i + 1))
 done
 
