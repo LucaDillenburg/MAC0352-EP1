d64de6fa2d5ef6cab01ef43a4ca8834f73b081c8
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-04-26 05:38:11 -0300

    fix scripts

--- scripts/start_publishers.sh
+++ scripts/start_publishers.sh
@@ -9,7 +9,7 @@ i=1
 while [ "$i" -le "$1" ]; do
     m="$(($i % $2))"
     echo "Started mosquitto_pub in topic 'topic_$m'"
-    ./mosquitto_pub -t "topic_$m" -m "Hello World" &
+    ./mosquitto_pub -t "topic_$m" -m "Hello World" -h 10.0.0.101 &
     i=$((i + 1))
 done
 
--- scripts/start_subscribers.sh
+++ scripts/start_subscribers.sh
@@ -9,7 +9,7 @@ i=1
 while [ "$i" -le "$1" ]; do
     m="$(($i % $2))"
     echo "Started mosquitto_sub in topic 'topic_$m'"
-    ./mosquitto_sub -t "topic_$m" &
+    ./mosquitto_sub -t "topic_$m" -h 10.0.0.101 &
     i=$((i + 1))
 done
 
